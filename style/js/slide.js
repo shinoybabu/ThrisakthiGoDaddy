2f80f2a72bc7a72a1b4bf2e86040b8b59f5222d6
Shinoy Babu <shinoybabu@hotmail.com>
2017-11-18 00:03:31 +0530

    changed

---
+++ style/js/slide.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  var selectIndicators = $('#myCarousel>.carousel-indicators>li');
+  selectIndicators.each(function(i){
+   // $(this).text((i+1) + ' / ' + selectIndicators.length);
+  }); 
+});
